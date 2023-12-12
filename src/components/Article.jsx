e7677d4b09c3904add4379b5bfd36315750e183f
Patrick Rigg <patrickarigg@gmail.com>
2023-12-12 11:30:00 +0000

    completes individual article page

---
+++ src/components/Article.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchArticleById } from "../api";
+
+function Article() {
+  const { article_id } = useParams();
+  const [currArticle, setCurrArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    fetchArticleById(article_id)
+      .then((foundArticle) => {
+        console.log(foundArticle);
+        setCurrArticle(foundArticle);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <article className="single-article">
+      <img src={currArticle.article_img_url}></img>
+      <h2>{currArticle.title}</h2>
+      <p>
+        <strong>By {currArticle.author}</strong>
+      </p>
+      <p>
+        {currArticle.comment_count} comments | {currArticle.votes} votes
+      </p>
+      <p>{currArticle.body}</p>
+    </article>
+  );
+}
+
+export default Article;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Home from "./Home";
 import { fetchAllArticlesSortedByDate } from "../api";
+import { Routes, Route } from "react-router-dom";
+import Article from "./Article";
 
 function Display() {
   const [articles, setArticles] = useState([]);
@@ -24,7 +26,10 @@ function Display() {
 
   return (
     <>
-      <Home articles={articles} />
+      <Routes>
+        <Route path="/" element={<Home articles={articles} />} />
+        <Route path="/articles/:article_id" element={<Article />} />
+      </Routes>
     </>
   );
 }
